9e2bce5d871286e815b5258bcdba2bcce4f5bf68
Saikat Das <sdsaikatdas2005@gmail.com>
2026-08-04 18:37:42 +0530

    Time: 0 ms (100%), Space: 10.4 MB (66.67%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans=0;
+        int mn=tickets[k];
+        for(int i=0;i<tickets.size();i++){
+            if(i<=k) ans+=min(mn,tickets[i]);
+            else ans+=min(mn-1,tickets[i]);
+        }
+        return ans;
+    }
+};
