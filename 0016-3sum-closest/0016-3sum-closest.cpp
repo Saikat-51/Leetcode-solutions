6e004660d5dae19181df71c1dd4740d3315a7903
Saikat Das <sdsaikatdas2005@gmail.com>
2026-08-12 19:42:15 +0530

    Time: 3 ms (98.17%), Space: 14.1 MB (15.14%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int closet = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (abs(sum - target) < abs(closet - target))
+                    closet = sum;
+                if (sum == target)
+                    return sum;
+                else if (sum < target)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return closet;
+    }
+};
