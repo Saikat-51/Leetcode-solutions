2dabf2999c197031c09ebc538c1760fcc68352eb
Saikat Das <sdsaikatdas2005@gmail.com>
2026-09-01 00:26:41 +0530

    Time: 3 ms (50.87%), Space: 18.1 MB (48.86%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        vector<pair<int, int>> arr;
+
+        for (auto it : freq) {
+            arr.push_back({it.first, it.second});
+        }
+
+        sort(arr.begin(), arr.end(), [](auto &a, auto &b) {
+            return a.second > b.second;
+        });
+
+        vector<int> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(arr[i].first);
+        }
+
+        return ans;
+    }
+};
